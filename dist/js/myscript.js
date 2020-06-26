aa0a48788aa9ef672c01b0452126bcf8cd8a017f
nayanpal777 <nayanpalmns@gmail.com>
2020-06-26 12:31:10 +0530

    update state Data

--- dist/js/myscript.js
+++ dist/js/myscript.js
@@ -28,8 +28,8 @@ app.controller('mycontroller', ['$scope', '$http', '$localStorage', function ($s
 }]);
 
 app.controller('india_Controller', ['$scope', '$http', '$localStorage', function ($scope, $http, $localStorage) {
-    
-    $scope.get_india_data = function (){
+
+    $scope.get_india_data = function () {
         if ($localStorage.India_data_storage) {
             $scope.india_data = $localStorage.India_data_storage;
             $scope.in_date = $scope.india_data.statewise['0'].lastupdatedtime;
@@ -39,9 +39,9 @@ app.controller('india_Controller', ['$scope', '$http', '$localStorage', function
                 $localStorage.India_data_storage = res.data;
                 alert('fetch data from api');
             });
-        }    
+        }
     };
-    
+
     $scope.refresh_India_Data = function () {
         $http.get('https://api.covid19india.org/data.json').then(function (res) {
             console.warn(res.data);
@@ -54,4 +54,32 @@ app.controller('india_Controller', ['$scope', '$http', '$localStorage', function
     $scope.orderby = function (item) {
         $scope.data_sort = item;
     }
+}]);
+
+app.controller('stateController', ['$scope', '$http', '$localStorage', function ($scope, $http, $localStorage) {
+    var u_state;
+    $scope.refresh_State_Data = function () {
+        $scope.date_time = new Date();
+        $http.get('https://api.covid19india.org/state_district_wise.json').then(function (res) {
+            console.warn(res.data);
+            $scope.state_data = res.data;
+            $localStorage.state_data_storage = res.data;
+        });
+    };
+
+    $scope.get_state_data = function () {
+        if ($scope.User_state == null | $scope.User_state == "") {
+            alert('Please Enter State');
+        } else if ($localStorage.state_data_storage) {
+            u_state = $scope.User_state;
+            var state_data = $localStorage.state_data_storage;
+            $scope.distict = state_data[$scope.User_state].districtData;
+        }
+    };
+    $scope.get_distict_data = function (key) {
+        $scope.district = key;
+        if($localStorage.state_data_storage){
+            $scope.data = $localStorage.state_data_storage[u_state].districtData[key];
+        }
+    };
 }]);
