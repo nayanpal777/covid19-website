e9126f5088c8114f6efa63f481d23cbe06747a40
nayanpal777 <nayanpalmns@gmail.com>
2020-06-27 09:55:08 +0530

    updated Files for navigation

--- dist/js/myscript.js
+++ dist/js/myscript.js
@@ -1,10 +1,22 @@
+//Application init
 var app = angular.module('myapp', ['ngStorage']);
+
+/* ***********************************************************
+    Array for chart View
+**************************************************************/
 var chart_date = [];
 var chart_cases = [];
 var chart_death = [];
 var chart_recoved = [];
 
+/* ***********************************************************
+    world Cases Controller
+**************************************************************/
 app.controller('mycontroller', ['$scope', '$http', '$localStorage', function ($scope, $http, $localStorage) {
+    
+    /*------------------------------------------------------------
+    ------> function to check data present in localStorage or Not
+    */
     $scope.getdatainfo = function () {
         if ($localStorage.datastorage) {
             $scope.data = $localStorage.datastorage;
@@ -19,6 +31,9 @@ app.controller('mycontroller', ['$scope', '$http', '$localStorage', function ($s
             });
         }
     };
+    /*----------------------------------------------------------
+    ------> function to refresh Data
+    */
     $scope.refreshData = function () {
         $http.get('https://api.covid19api.com/summary').then(function (res) {
             $scope.data = res.data;
@@ -26,13 +41,23 @@ app.controller('mycontroller', ['$scope', '$http', '$localStorage', function ($s
             $scope.getdatainfo();
         });
     };
+    /*----------------------------------------------------------
+    ------> function to Order the Data
+    */
     $scope.orderbyme = function (item) {
         $scope.item = item;
     }
 }]);
 
-app.controller('india_Controller', ['$scope', '$http', '$localStorage', function ($scope, $http, $localStorage) {
 
+/* ***********************************************************
+    India Cases Controller
+**************************************************************/
+app.controller('india_Controller', ['$scope', '$http', '$localStorage', function ($scope, $http, $localStorage) {
+    
+    /*------------------------------------------------------------
+    ------> function to check data present in localStorage or Not
+    */
     $scope.get_india_data = function () {
         if ($localStorage.India_data_storage) {
             $scope.india_data = $localStorage.India_data_storage;
@@ -46,6 +71,9 @@ app.controller('india_Controller', ['$scope', '$http', '$localStorage', function
         }
     };
 
+    /*------------------------------------------------------------
+    ------> function to Refresh The Data
+    */
     $scope.refresh_India_Data = function () {
         $http.get('https://api.covid19india.org/data.json').then(function (res) {
             console.warn(res.data);
@@ -55,10 +83,15 @@ app.controller('india_Controller', ['$scope', '$http', '$localStorage', function
             alert('refresh Button call');
         });
     };
+    /*------------------------------------------------------------
+    ------> function to Order the Data
+    */
     $scope.orderby = function (item) {
         $scope.data_sort = item;
     }
-
+    /*------------------------------------------------------------
+    ------> function for chart Data
+    */
     $scope.chartfun = function () {
         if ($localStorage.India_data_storage) {
             $scope.chart_data = $localStorage.India_data_storage;
@@ -72,8 +105,16 @@ app.controller('india_Controller', ['$scope', '$http', '$localStorage', function
     };
 }]);
 
+/* ***********************************************************
+    District Cases Controller
+**************************************************************/
 app.controller('stateController', ['$scope', '$http', '$localStorage', function ($scope, $http, $localStorage) {
+    //variable ------------
     var u_state;
+
+    /*------------------------------------------------------------
+    ------> function to Refresh the Distict Data
+    */
     $scope.refresh_State_Data = function () {
         $scope.date_time = new Date();
         $http.get('https://api.covid19india.org/state_district_wise.json').then(function (res) {
@@ -82,6 +123,10 @@ app.controller('stateController', ['$scope', '$http', '$localStorage', function 
             $localStorage.state_data_storage = res.data;
         });
     };
+
+    /*------------------------------------------------------------
+    ------> function to get Distict from the Enter State
+    */
     $scope.get_state_data = function () {
         if ($scope.User_state == null | $scope.User_state == "") {
             alert('Please Enter State');
@@ -91,6 +136,10 @@ app.controller('stateController', ['$scope', '$http', '$localStorage', function 
             $scope.distict = state_data[$scope.User_state].districtData;
         }
     };
+    
+    /*------------------------------------------------------------
+    ------> function to get Caseses Data from selected District
+    */
     $scope.get_distict_data = function (key) {
         $scope.district = key;
         if($localStorage.state_data_storage){
